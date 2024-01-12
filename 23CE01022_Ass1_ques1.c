b2f75de89f7c236abbd70ae65390386821edc7f1
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-01-12 14:01:32 +0530

    Add files via upload

---
+++ 23CE01022_Ass1_ques1.c
@@ -0,0 +1,9 @@
+/*c program to print your name,roll.no and branch*/
+#include<stdio.h>
+int main()
+{
+printf("\n my name is  Gurrala Venkata Sri Krishna Balu");
+printf("\n Roll.no:23CE01022");
+printf("\n Branch is civil engineering");
+return 0;
+}
