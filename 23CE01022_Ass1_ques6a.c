64ae2af73648fe55edc883ec3ce520c834d509f3
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-01-12 16:14:09 +0530

    Add files via upload

---
+++ 23CE01022_Ass1_ques6a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int a=2147483648;
+   int b=2147483649;
+   int c=2147483650;
+   printf("\n%d",a);
+   printf("\n%d",b);
+   printf("\n%d",c);
+    return 0;
+}
