fad688d1993eecfcd0456c7b1a4ff278feb77099
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-01-12 23:04:47 +0530

    Add files via upload

---
+++ 23CE01022_Ass1_ques6c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n limit of int = -2147483648 to 2147483647");
+    printf("\n limit of char = -128 to 127");
+    printf("\n limit of long int = -9223372036854775808 to 9223372036854775807");
+    printf("\n limit of short int = -32768 to 32767");
+    printf("\n limit of float = 1.2E-38 to 3.4E+38");
+    printf("\n limit of double = 2.3E-308 to 1.7E+308");
+    printf("\n limit of long double = 3.4E-4932 to 1.1E+4932");
+    return 0;
+}
