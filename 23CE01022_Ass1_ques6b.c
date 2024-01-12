750b6f56b9592fc1a7059329658513c573d6d513
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-01-12 22:49:07 +0530

    Add files via upload

---
+++ 23CE01022_Ass1_ques6b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    long long a=9223372036854775807;
+    printf("\n %lld",a);
+    long b=2147483647;
+    printf("\n %ld",b);
+    short c=32767;
+    printf("\n %hd",c);
+    return 0;
+}
