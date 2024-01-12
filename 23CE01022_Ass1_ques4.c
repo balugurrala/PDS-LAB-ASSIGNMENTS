066c9aff6b988e53c53302bb8994ab919e6d6ef8
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-01-12 15:15:24 +0530

    Add files via upload

---
+++ 23CE01022_Ass1_ques4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{ 
+    float e=2.718281828;
+    printf("\n %.0f",round(e));
+    printf("\n %.1f",round(e));
+    printf("\n %.1f",e);
+    printf("\n %.2f",e);
+    printf("\n %.6f",e);
+    printf("\n %.6f",e);
+    printf("\n %.7f",e);
+    return 0;
+}
