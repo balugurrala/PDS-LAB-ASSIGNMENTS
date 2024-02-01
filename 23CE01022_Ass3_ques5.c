bd8a1b4a30f3b1c2fcac4d8055a54db07aff5cea
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-02-01 23:55:54 +0530

    Add files via upload

---
+++ 23CE01022_Ass3_ques5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int x;
+    printf("enter no of days late");
+    scanf("%d",&x);
+    if(x==0)
+    printf("no fine");
+   else if(x>0&&x<=5)
+    printf("1 rupee fine");
+   else if(x>5&&x<=10)
+    printf("2 rupee fine");
+   else if(x>10)
+    printf("ur membership is cancelled");
+    return 0;
+}
