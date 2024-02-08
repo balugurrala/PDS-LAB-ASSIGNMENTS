7c100c22ca1ea80e0c876e5ebf4d5a34f345334a
balugurrala <155727964+balugurrala@users.noreply.github.com>
2024-02-08 23:37:31 +0530

    Update 23CE01022_Ass4_ques3.c

--- 23CE01022_Ass4_ques3.c
+++ 23CE01022_Ass4_ques3.c
@@ -6,19 +6,16 @@ int main() {
     printf("Enter credit score: ");
     scanf("%d", &creditScore);
 
-    printf("Enter current balance: $");
+    printf("Enter current balance: ");
     scanf("%d", &currentBalance);
-
+    int interest;
     if (creditScore < 600) {
-        interestRate = 0.15;
+        printf("interest=%d",interest=currentBalance*15/100);
     } else if (creditScore >= 600 && creditScore <= 750) {
-        interestRate = 0.12;
+        printf("interest=%d",interest=currentBalance*12/100);
     } else {
-        interestRate = 0.10;
+        printf("interest=%d",interest=currentBalance*10/100);
     }
-
-    int interest = currentBalance * interestRate;
-    printf("The calculated interest is: %d\n", interest);
 
     return 0;
 }
